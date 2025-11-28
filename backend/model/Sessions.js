8452ef66e9b5b00e1e2f1aa78b4d6bddf3e0bf64
Ghada31-alghamdi <s202269300@kfupm.edu.sa>
2025-11-28 22:43:13 +0300

    craeated sample data for the Backend for Sessions course and tutor

---
+++ backend/model/Sessions.js
@@ -0,0 +1,34 @@
+export const Sessions = [
+  {
+    sessionId: "SES001",
+    id: "SWE 353",
+    toterId: "TTR001",
+    toter: "By Mohamed Alzhrane",
+    time: "6:00 PM",
+    description: "Overview of web engineering basics.",
+  },
+  {
+    sessionId: "SES002",
+    id: "ICS 104",
+    toterId: "TTR002",
+    toter: "By Norah Alghamdi",
+    time: "7:00 PM",
+    description: "Intro to Python programming.",
+  },
+  {
+    sessionId: "SES003",
+    id: "ICS 500",
+    toterId: "TTR003",
+    toter: "By Hamad Alharbi",
+    time: "6:30 PM",
+    description: "Brief on software methodologies.",
+  },
+  {
+    sessionId: "SES004",
+    id: "MATH 201",
+    toterId: "TTR004",
+    toter: "By Ghada Alghamdi",
+    time: "3:00 PM",
+    description: "Multivariable calculus essentials.",
+  },
+];
