fc497aeb7403190a708e1ee34771259093f060cc
darkwinTech <s202257160@kfupm.edu.sa>
2025-12-01 15:39:30 +0300

    supportRoute

--- backend/dataSeed/seedUsers.js
+++ backend/dataSeed/seedUsers.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const User = require("./model/User");
+const User = require("../model/User");
 
 async function seed() {
 	try {
